83f6c03317c164d98f097ade017d7fca4dd0531a
Aishwarya Joy <aishwarya.joy@vonnue.com>
2021-12-09 12:23:18 +0530

    useState array method tutorial

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import HookCounter from './component/hookCounter';
 import HookCounterTwo from './component/hookCounterTwo';
 import HookCounter3 from './component/hookCounter3';
+import HookCounter4 from './component/hookCounter4';
 
 function App() {
   return (
     <div className="App">
       {/* <HookCounter/>
       <HookCounterTwo/> */}
-      <HookCounter3/>
+      {/* <HookCounter3/> */}
+      <HookCounter4/> 
     </div>
   );
 }
